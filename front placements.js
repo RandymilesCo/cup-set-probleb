b2e83664b28dc328898f7f27a55550547b3d2050
Randy Miles <155011848+RandymilesCo@users.noreply.github.com>
2023-12-27 20:19:49 +0200

    Create front placements.js

---
+++ front placements.js
@@ -0,0 +1,18 @@
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/\\.next/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    /* Use babel-jest to transpile tests with the next/babel preset
+        https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+    '^.+\\.(js|jsx|ts|tsx|svg)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$']
+
+ moduleDirectories: ['node_modules', '<rootDir>/node_modules', '.'],
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    /* Handle CSS imports (with CSS modules)
+
+        https://jestjs.io/docs/webpack#mocking-css-modules */
+    '\\.svg$': '<rootDir>/__mocks__/svg.js',
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
